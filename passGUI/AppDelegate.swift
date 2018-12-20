c32be8967f7473810b7badbf9ab2257f0b26cd36
Sid Raval <sid@thoughtbot.com>
2018-12-20 18:53:00 -0500

    Print simulator documents directory for inspection

--- passGUI/AppDelegate.swift
+++ passGUI/AppDelegate.swift
@@ -16,6 +16,12 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
 
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
         // Override point for customization after application launch.
+        #if targetEnvironment(simulator)
+        if let documentsPath = FileManager.default.urls(for: .documentDirectory, in: .userDomainMask).first?.path {
+            print("Documents Directory: \(documentsPath)")
+        }
+        #endif
+
         return true
     }
 
